710b8e58a4466719dc1a0afdf9c02c7cd4fc8f62
warbr1ng3r <i.warbr1ng3r@gmail.com>
2023-05-05 20:26:17 +0300

    Initial commit v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import usePluginImport from 'vite-plugin-importer';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [
+    usePluginImport({
+      libraryName: '@mui/material',
+      libraryDirectory: '',
+      camel2DashComponentName: false
+    }),
+    react(),
+    tsconfigPaths()
+  ]
 });
