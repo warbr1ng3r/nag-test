87e981f81a458765249d94d9586cf17b6d9f4295
warbr1ng3r <i.warbr1ng3r@gmail.com>
2023-05-06 00:33:53 +0300

    Fix style, add theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,15 @@
-import { useState } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
 
 import './App.css';
+import { theme } from './theme';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <h1>Vite + React</h1>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    </ThemeProvider>
   );
 }
 
